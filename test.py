2f2ab9748fdd034ec057380eded14fb050022793
Dikshant Joshi <91108862+Dikshant20011891@users.noreply.github.com>
2023-06-02 12:39:32 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,34 +1,3 @@
-# import requests
-# import json
-# import argparse
-
-# def get_weather(city):
-#     url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid=cbf04a5926dc62ad1fd20d61fa66b75e&units=metric"
-#     response = requests.get(url)
-#     data = json.loads(response.text)
-
-#     # print(json.dumps(data, indent=4))
-    
-#     if data["cod"] == "404":
-#         print("City not found.")
-#     else:
-#         temperature = data["main"]["temp"]
-#         weather_description = data["weather"][0]["description"]
-#         print(f"Weather in {city}: {weather_description}")
-#         print(f"Temperature: {temperature}°C")
-
-# def main():
-#     parser = argparse.ArgumentParser(description="Take the city name input")
-#     parser.add_argument("city")
-
-#     args = parser.parse_args()
-#     city = args.city
-
-#     get_weather(city)
-
-# if __name__ == "__main__":
-#     main()
-
 import requests
 import json
 import argparse
